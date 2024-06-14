0ee8624d98ee113b9d798378260a9c4963753b85
Adham Khalifa <akhalifae@conncoll.edu>
2024-06-14 01:46:05 -0400

    Deployment

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import "./App.css";
-import logo from "./logo.png";
+import "../App.css";
+import logo from "../logo.png";
 
 const Main = () => {
   return (
